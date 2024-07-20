e41abd3c6907161e9ae9d73293a2a63dcdbcc228
Raymond Teng Toh Zi <raintaroteng@gmail.com>
2024-07-20 09:28:34 +0800

    finalize

--- WebEngine.py
+++ WebEngine.py
@@ -18,6 +18,7 @@ class GUI():
         self.button2.pack(padx=20,pady=10)
         self.m.geometry("300x150")
         self.AutoMouse = AutoDragger()
+        self.m.resizable(False,False)
         self.path = path
 
     def Start(self):
@@ -25,50 +26,33 @@ class GUI():
         self.browser.get(r"https://www.chess.com/play")
         
     def run_bot_white(self):
-        self.engine = Stockfish(path=self.path)
-        board = chess.Board()
-        moves = []
+        self.setup()
         best_move = self.engine.get_best_move()
-        print("the best move is " + best_move)
         self.AutoMouse.Drag((self.AutoMouse.convertStrToPositionWhite(best_move[:2])),self.AutoMouse.convertStrToPositionWhite(best_move[2:5]))
-        while(True):
-            game_over = self.browser.find_elements(By.CSS_SELECTOR, '[class*="game-over-arena-details-component"]')
-            print(game_over[0].get_attribute("style") == '')
-            elements = self.browser.find_elements(By.CSS_SELECTOR, '[class*="node-highlight-content"][class*="offset-for-annotation-icon"]')
-            if(len(elements) != len(moves)):
-                try:
-                    moves.append(elements[-1].find_element(By.CSS_SELECTOR,"[class*=icon-font-chess]").get_attribute("data-figurine")+elements[-1].text)
-                except:
-                    moves.append(elements[-1].text)
-                board.push_xboard(moves[-1])
-                if(board.is_checkmate()):
-                    print("Congrats!! CHECKMATE!!")
-                    break
-                try:
-                    if(elements[-1].find_element(By.CSS_SELECTOR,"game-over-message-component").text.lower() == "game over"):
-                        print("The Game Ended.")
-                        break
-                except:
-                    pass
-                self.engine.make_moves_from_current_position([board.peek()])
-                if(len(moves)%2 == 0 and len(moves) != 1):
-                    best_move = self.engine.get_best_move()
-                    print("the best move is " + best_move)
-                    self.AutoMouse.Drag((self.AutoMouse.convertStrToPositionWhite(best_move[:2])),self.AutoMouse.convertStrToPositionWhite(best_move[2:]))
+        self.bot_logic(self.AutoMouse.convertStrToPositionWhite,True)
+
+                
 
     def run_bot_black(self):
+        self.setup()
+        self.bot_logic(self.AutoMouse.convertStrToPositionBlack,False)
+
+    def setup(self):
         self.engine = Stockfish(path=self.path)
-        board = chess.Board()
-        moves = []
+        self.board = chess.Board()
+        self.moves = []
+
+    def bot_logic(self,function,condition):
+
         while(True):
             elements = self.browser.find_elements(By.CSS_SELECTOR, '[class*="node-highlight-content"][class*="offset-for-annotation-icon"]')
-            if(len(elements) != len(moves)):
+            if(len(elements) != len(self.moves)):
                 try:
-                    moves.append(elements[-1].find_element(By.CSS_SELECTOR,"[class*=icon-font-chess]").get_attribute("data-figurine")+elements[-1].text)
+                    self.moves.append(elements[-1].find_element(By.CSS_SELECTOR,"[class*=icon-font-chess]").get_attribute("data-figurine")+elements[-1].text)
                 except:
-                    moves.append(elements[-1].text)
-                board.push_xboard(moves[-1])
-                if(board.is_checkmate()):
+                    self.moves.append(elements[-1].text)
+                self.board.push_xboard(self.moves[-1])
+                if(self.board.is_checkmate()):
                     print("Congrates!! CHECKMATE!!")
                     break
                 try:
@@ -77,8 +61,12 @@ class GUI():
                         break
                 except:
                     pass
-                self.engine.make_moves_from_current_position([board.peek()])
-                if(len(moves)%2 == 1):
-                    best_move = self.engine.get_best_move()
-                    print("the best move is " + best_move)
-                    self.AutoMouse.Drag((self.AutoMouse.convertStrToPositionBlack(best_move[:2])),self.AutoMouse.convertStrToPositionBlack(best_move[2:]))
+                self.engine.make_moves_from_current_position([self.board.peek()])
+                if(condition == False):
+                    if(len(self.moves)%2 == 1):
+                        best_move = self.engine.get_best_move()
+                        self.AutoMouse.Drag(function((best_move[:2])),function(best_move[2:]))
+                else:
+                    if(len(self.moves)%2 == 0 and len(self.moves) != 1):
+                        best_move = self.engine.get_best_move()      
+                        self.AutoMouse.Drag(function((best_move[:2])),function(best_move[2:]))
